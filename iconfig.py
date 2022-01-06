d8a8eb82fe68bb1a69bde2a655075dad47a93561
Volodymyr Savchenko <contact@volodymyrsavchenko.com>
2022-01-06 11:58:15 +0100

    recovered reconstrcutin in rsp as in lut2

--- iconfig.py
+++ iconfig.py
@@ -5,11 +5,12 @@ settings = Dynaconf(
     envvar_prefix="INTEGRAL",
     settings_files=[
         os.path.join(os.getenv("HOME"), '.integral-settings.toml'), 
-        ],
+    ],
 )
 
 settings.validators.register(
-    Validator("REP_BASE_PROD", must_exist=True, eq="/isdc/arc/rev_3")
+    Validator("REP_BASE_PROD", must_exist=True),
+    Validator("IC_BASE", must_exist=True)
 )
 
 settings.validators.validate()
